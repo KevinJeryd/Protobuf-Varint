290b3327ff820f40bc73c52e6742e40793ece48d
Kevin Jeryd <kevinjeryd01@gmail.com>
2024-09-11 23:54:20 +0200

    Add updated encode function and new decode function

--- include/varint.h
+++ include/varint.h
@@ -8,6 +8,7 @@
 std::vector<unsigned char> readFile(const std::string& path);
 uint64_t bytesToInt(const std::vector<unsigned char>& bytes);
 std::vector<unsigned char> encodeVarint(uint64_t value);
-
+uint64_t decodeVarint(const std::vector<unsigned char>& bytes);
+int roundtripTest(const uint64_t varint);
 
 #endif // VARINT_H
--- main.cpp
+++ main.cpp
@@ -6,18 +6,13 @@
 #include "include/varint.h"
 
 int main() {
-   std::vector<unsigned char> varint = readFile("../../varint/1.uint64");
+   std::vector<unsigned char> varint1 = readFile("../../varint/1.uint64");
+   std::vector<unsigned char> varint150 = readFile("../../varint/150.uint64");
+   std::vector<unsigned char> varintMax = readFile("../../varint/maxint.uint64");
 
-   for (auto byteNum : varint) {
-      printf("%02x", byteNum);
-      printf(" ");
-   }
+   uint64_t varintDec = bytesToInt(varint150);
 
-   std::cout << std::endl;
-
-   uint64_t varintDec = bytesToInt(varint);
-
-   std::cout << "Value to be decoded: " << varintDec << std::endl;
+   std::cout << "Value to be encoded: " << varintDec << std::endl;
 
    std::vector<unsigned char> encodedVarint = encodeVarint(varintDec);
 
@@ -26,18 +21,68 @@ int main() {
    for (auto byteNum : encodedVarint) {
       printf("%02x", byteNum);
    }
+   std::cout << std::endl;
+
+   std::vector<unsigned char> encodedVarint2 {0x01, 0x01};
+
+   uint64_t decodedVarint = decodeVarint(encodedVarint2);
+
+   printf("Decoded Value: %d", decodedVarint);
+   std::cout << std::endl;
+
+   uint64_t varint1Dec = bytesToInt(varint1);
+   uint64_t varint150Dec = bytesToInt(varint150);
+   uint64_t varintMaxDec = bytesToInt(varintMax);
+
+   std::cout << "roundtripTest for varint1: " << roundtripTest(varint1Dec) << std::endl;
+   std::cout << "roundtripTest for varint150: " << roundtripTest(varint150Dec) << std::endl;
+   std::cout << "roundtripTest for varintMax: " << roundtripTest(varintMaxDec) << std::endl;
 
     return 0;
+}
+
+int roundtripTest(const uint64_t varint) {
+   std::vector<unsigned char> encodedVarint = encodeVarint(varint);
+   uint64_t decodedVarint = decodeVarint(encodedVarint);
+
+   return varint == decodedVarint;
 }
 
 std::vector<unsigned char> encodeVarint(uint64_t value) {
    std::vector<unsigned char> result;
 
-   for (int i = 0; i < sizeof(value)/7; ++i) {
-      unsigned char bytePortion = value << (i * 7);
+   while (value > 0) {
+      unsigned char bytePortion = value & 0x7F;
+      value >>= 7;
+      
+      if (value > 0) {
+         bytePortion |= 0x80;
+      }
+
       result.push_back(bytePortion);
-      result.push_back(0b1);
    }
+
+   return result;
+}
+
+uint64_t decodeVarint(const std::vector<unsigned char>& bytes) {
+   uint64_t result{};
+
+   std::cout << "Bytes to be decoded: ";
+   for (auto byte : bytes) {
+      printf("%02x", byte);
+   }
+   std::cout << std::endl;
+
+   int i = 0;
+
+   // If the MSB is set, then we have more bytes to read
+   // Technically we don't need to check this since we have a byte sequence as input
+   // So we can just use the size of the vector ig
+   do {
+      result |= (bytes[i] & 0x7F) << (i * 7);
+      ++i;
+   } while (bytes[i-1] & 0x80); // Really scuffed way of doing it, but I just wanted to check the MSB... 
 
    return result;
 }
@@ -71,13 +116,9 @@ std::vector<unsigned char> readFile(const std::string& path) {
       // go back to the start
       file.seekg(0, std::ios::beg);
 
-      std::cout << "start: " << start << std::endl;
-      std::cout << "end: " << end << std::endl;
-
-      std::cout << "File size is: " << (end - start) << " bytes." << std::endl;
-
       // create a vector to hold the data that
-      // is resized to the total size of the file    
+      // is resized to the total size of the file
+      // Not really needed since all input are uint64    
       std::vector<unsigned char> contents;
       contents.resize(static_cast<size_t>(end - start));
 
